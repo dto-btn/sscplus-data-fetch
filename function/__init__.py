411c16c49a4cb7571f8e6415c3fc7ee7362e8493
Guillaume Turcotte <leguille.ftw@gmail.com>
2023-10-03 08:59:05 -0400

    added redirect support

--- function/__init__.py
+++ function/__init__.py
@@ -1,22 +1,35 @@
 from azure.core.pipeline import Pipeline
+import pip_system_certs.wrapt_requests
 from azure.core.pipeline.transport import HttpRequest, HttpResponse
-from azure.core.pipeline.policies import BearerTokenCredentialPolicy, HttpLoggingPolicy
+from azure.core.pipeline.policies import BearerTokenCredentialPolicy, HttpLoggingPolicy, RedirectPolicy  
 from azure.core.pipeline.transport import RequestsTransport
 from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
 import logging
+from azure.core.pipeline.policies import UserAgentPolicy, NetworkTraceLoggingPolicy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logger = logging.getLogger("azure.identity")
 logger.setLevel(logging.DEBUG)
 
 credential = DefaultAzureCredential()
-credential_policy = BearerTokenCredentialPolicy(credential, 'https://plus-dev.ssc-spc.gc.ca/.default')
+credential_policy = BearerTokenCredentialPolicy(credential, 'https://graph.microsoft.com/.default')
 
 policies = [
+    UserAgentPolicy("SSCPlusDrupal"),  
+    NetworkTraceLoggingPolicy(), 
     credential_policy,
-    HttpLoggingPolicy()
+    HttpLoggingPolicy(),
+    RedirectPolicy()
 ]
 
-transport = RequestsTransport()#connection_verify=False)
+transport = RequestsTransport(connection_verify=False)
 pipeline = Pipeline(transport=transport, policies=policies) 
 request = HttpRequest('GET', 'https://plus-dev.ssc-spc.gc.ca/rest/all-ids')
 response = pipeline.run(request)
+
+# Get the response  
+print("RESPONSE HTTP CODE: " + str(response.http_response.status_code)) 
+print("RESPONSE BODY: " + str(response.http_response.headers))
+print(response.http_response.text())
